6073d0abee4fd7f3c8bc2bfdfc623fca26ac7abe
Adri726-av <aleovel726@g.educaand.es>
2026-01-12 10:58:14 +0100

    Subo

--- UD 4. ETAPA FÍSICA/Scripts/Ejercicio3.sql
+++ UD 4. ETAPA FÍSICA/Scripts/Ejercicio3.sql
@@ -27,3 +27,30 @@ CREATE TABLE Tienda(
     Tipo varchar2(14) NOT NULL,
     CONSTRAINT ck_Tipotienda CHECK(Tipo IN ('On-line', 'Física'))
 );
+
+CREATE TABLE Producto(
+	IdProducto number(14) PRIMARY KEY,
+	Nombre varchar2(32) NOT NULL,
+    -- Tenemos en cuenta los VAT internacionales (ES-CIF)
+	SKU varchar2(32) NOT NULL UNIQUE,
+	Marca varchar2(32) NOT NULL,
+	Modelo varchar2(32) NOT NULL,
+    -- Precio estándar de 6 dígitos + 2 decimales
+	Precio number(6,2),
+    Categoria varchar2(32) DEFAULT 'Móviles',
+    -- Los estados posibles son: 'B' Borrador (por defecto), 'P' Publicado y 'E' Eliminado
+    Estado char(1) DEFAULT 'B',
+    UltimaModificacion DATE DEFAULT SYSDATE,
+    CONSTRAINT ck_CategoriaProducto CHECK(Categoria IN('Móviles','Carcasas','Cargadores','Tarjetas SIM', 'Accesorios')),
+    CONSTRAINT ck_EstadoProducto CHECK(Estado IN('B','P','E'))
+);
+
+CREATE TABLE Compra (
+    IdCompra number(14),
+    IdProveedor number(14),
+    IdTienda number(14),
+    Fecha DATE DEFAULT SYSDATE,
+    CONSTRAINT pk_compra PRIMARY KEY (IdCompra, IdProveedor, IdTienda),
+    CONSTRAINT fk_CompraProveedor FOREIGN KEY (IdProveedor) REFERENCES Proveedor(IdProveedor) ON DELETE CASCADE,
+    CONSTRAINT fk_Compratienda FOREIGN KEY (IdTienda) REFERENCES Tienda(IdTienda) ON DELETE CASCADE
+);
